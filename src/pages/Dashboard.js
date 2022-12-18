8a44e59729ed664fb13e6661893f11a9e88181eb
jithin-j <jithinjagadeesh1@gmail.com>
2022-12-18 07:58:31 +0530

    Connect metamask

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../components/Navbar";
 import image from "../images/music.svg";
 import "../styles/dashboard.css";
 
 const Dashboard = () => {
+    const [walletAddress, setWalletAddress] = useState("false")
+    async function requestAccount() {
+        console.log('requesting account');
+        if(window.ethereum){
+            console.log('detected');
+            try {
+                const accounts = await window.ethereum.request({
+                    method: "eth_requestAccounts"
+                });
+                setWalletAddress(accounts[0]);
+                console.log(accounts)
+            } catch (error) {
+                console.log(error);
+            }
+        }else{
+            console.log('wallet not detected');
+        }
+    }
   return (
     <>
       <Navbar />
       <div className="dashboard-header">
         <div className="dashboard-content">
           <div id="heading">Dashboard</div>
-          <button className="button">Connect with Metamask</button>
+          {walletAddress === "false" && (
+            <button className="button" onClick={requestAccount}>
+              Connect with Metamask
+            </button>
+          )}
+          {walletAddress !== "false" && (
+            <h3 style={{ color: "#E45E84" }}>{walletAddress}</h3>
+          )}
         </div>
         <div className="dashboard-content">
           <img src={image} alt="" style={{ width: "15rem" }} />
